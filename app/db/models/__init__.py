547e3ab770537cd2dbe5abc772f51ddc5193a259
Hicran Temel <hicrantemel2004@gmail.com>
2025-08-08 18:40:53 +0300

    init.py model eklendi

--- app/db/models/__init__.py
+++ app/db/models/__init__.py
@@ -2,18 +2,14 @@ from .MainCategory import MainCategory
 from .Campaign import Campaign
 # varsa diğer modelleri de buraya ekle
 
-# from .about_contents import About_Contents
-# from .TariffCategory import TariffCategory
-# from .channel_list import channel_list
-# from .campaign_commitment import CampaignCommitment
-# from .campaign_features import CampaignFeatures
-# from .commitment_period import CommitmentPeriod
-# from .device_commitment import DeviceCommitment
-# from .CategoryColumn import CategoryColumn
-# from .devices import Devices
-# from .TariffValue import TariffValue
-# from .Tariff import Tariff
-# from .TariffColum import TariffColum
-# from .TariffCell import TariffCell
-# from .TariffCategory import TariffCategory
+from .about_contents import About_Contents
+from .TariffCategory import TariffCategory
+from .channel_list import channel_list
+from .campaign_commitment import CampaignCommitment
+from .campaign_features import CampaignFeatures
+from .commitment_period import CommitmentPeriod
+from .device_commitment import DeviceCommitment
+from .CategoryColumn import CategoryColumn
+from .devices import Devices
+
 
