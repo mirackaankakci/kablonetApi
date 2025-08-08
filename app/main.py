eea113b48f86c895fc3263eddaefd1206c28c56d
miraç kaan kakcı <kaankakcii34@gmail.com>
2025-08-08 17:16:45 +0300

    main güncellendi

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
-from app.api.v1 import main_category_routes
-from app.api.v1 import channel_list_routes
-from app.api.v1 import campaign_routes
+from app.api.v1 import (
+    main_category_routes,
+    channel_list_routes,
+    campaign_routes,
+    campaign_features_routes,
+    campaign_commitment_routes,
+    commitment_period_routes,
+    devices_routes,
+    device_commitment_routes,
+    about_contents_routes,
+    tariff_category_routes,
+)
 from app.db.database import engine
 from app.db.base import Base
 
@@ -10,6 +19,14 @@ app = FastAPI()
 # Tabloları oluştur (ilk çalıştırmada)
 Base.metadata.create_all(bind=engine)
 
+# Router registrations
 app.include_router(main_category_routes.router)
 app.include_router(channel_list_routes.router)
 app.include_router(campaign_routes.router)
+app.include_router(campaign_features_routes.router)
+app.include_router(campaign_commitment_routes.router)
+app.include_router(commitment_period_routes.router)
+app.include_router(devices_routes.router)
+app.include_router(device_commitment_routes.router)
+app.include_router(about_contents_routes.router)
+app.include_router(tariff_category_routes.router)
